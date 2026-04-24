59a7dca601d1507a37e5584060a8f2e562d599fc
Aseem Timalsina <39004117+aseemtim@users.noreply.github.com>
2026-04-24 09:59:58 -0400

    Implement blockade mechanic

--- scripts.js
+++ scripts.js
@@ -1,26 +1,43 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+const map = new Map();
+for (i=0; i<canvas.width; i++) {
+    for (j=0; j<canvas.height; j++) {
+        map.set(`${i}${j}`, 0)
+    }
+}
+
+function createBlockade(x,y,w,h){
+    for (i=x; i<=x+w; i++) {
+        for (j=y; j<=y+h; j++) {
+            map.set(`${i}${j}`, true);
+        }
+    }
+    ctx.fillStyle = "rgb(167, 175, 234)";
+    ctx.fillRect(x,y,w,h);
+    
+}
+
+function hasBlockade(x,y){
+    return map.get(`${x}${y}`)
+}
+
 const player = {
     x: 50,
     y: 50,
-    speed: 5,
+    height:4,
+    width:4,
+    speed:1,
     draw() {
-        ctx.beginPath();
-        ctx.moveTo(this.x,this.y);
-        ctx.lineTo(this.x, this.y+4); 
-        ctx.lineTo(this.x+4, this.y+4); // 4 is player size
-        ctx.lineTo(this.x+4, this.y);
-        ctx.closePath();
-        ctx.fillStyle = this.color;
-        ctx.fill();
+        ctx.fillRect(this.x,this.y,this.height,this.width);
     }
 }
 
 const enemy = {
     x:280,
     y:280,
-    speed: 2,
+    speed: 0,
     draw() {
         ctx.beginPath();
         ctx.arc(this.x,this.y,2,0,Math.PI*2,false);
@@ -29,10 +46,10 @@ const enemy = {
 }
 
 function checkBoundary(x, y) {
-    if (x >= canvas.width) return [canvas.width - 4, y]
+    if (x >= canvas.width) return [canvas.width + 1 - player.width, y]
     if (x < 0) return [0, y]
 
-    if (y >= canvas.height) return [x, canvas.width - 4]
+    if (y >= canvas.height) return [x, canvas.width + 1 - player.height]
     if (y < 0) return [x, 0]
     return [x,y];
 }
@@ -53,6 +70,8 @@ function drawCloser() {
 }
 player.draw();
 enemy.draw();
+createBlockade(150,172,10,10);
+
 const keys = {};
 
 document.addEventListener("keydown", (e) => keys[e.key] = true);
@@ -61,12 +80,57 @@ document.addEventListener("keyup", (e) => keys[e.key] = false);
 function gameLoop() {
     ctx.reset();
 
-    if (keys["w"]) player.y -= player.speed;
-    if (keys["a"]) player.x -= player.speed;
-    if (keys["s"]) player.y += player.speed;
-    if (keys["d"]) player.x += player.speed;
+    if (keys["w"]){
+        canMove = true
+        for(i=player.x; i<=player.x+player.width; i++){
+            if (hasBlockade(i, player.y-1)) {
+                canMove = false
+            }
+        }
+        if (canMove) {
+            player.y -= player.speed;
+        }
+    }
+        
+    if (keys["a"]){
+        canMove = true
+        for(i=player.y; i<=player.y+player.height; i++){
+            if (hasBlockade(player.x-1, i)) {
+                canMove = false
+            }
+        }
+        if (canMove) {
+            player.x -= player.speed;
+        }
+    }
+
+
+    if (keys["s"]){
+        canMove = true
+        for(i=player.x; i<=player.x+player.width; i++){
+            if (hasBlockade(i, player.y+player.height+1)) {
+                canMove = false
+            }
+        }
+        if (canMove) {
+            player.y += player.speed;
+        }
+    }
+
+    if (keys["d"]){
+        canMove = true
+        for(i=player.y; i<=player.y+player.height; i++){
+            if (hasBlockade(player.x+player.width+1, i)) {
+                canMove = false
+            }
+        }
+        if (canMove) {
+            player.x += player.speed;
+        }
+    }
     player.x = checkBoundary(player.x,player.y)[0]
     player.y = checkBoundary(player.x,player.y)[1]
+    createBlockade(150,172,10,10);
     player.draw();
     drawCloser();
     enemy.draw();
